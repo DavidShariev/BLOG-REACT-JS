0873650a353b0a67d40394e70c887fd72241a3b9
David <david.shariev.2002@gmail.com>
2022-06-24 11:26:39 +0300

    5. refactoring

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ import checkAuth from "./utils/checkAuth.js";
 
 import UserModel from "./models/User.js";
 import User from "./models/User.js";
+import * as UserControllers from "./controllers/UserController.js";
 
 const app = express();
 const port = 4444;
@@ -31,118 +32,13 @@ app.get("/", (req, res) => {
 });
 
 //регистрация
-app.post("/auth/register", registerValidator, async (req, res) => {
-  try {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      //ответ 400 - ошибка запроса
-      return res.status(400).json(errors.array());
-    }
-
-    const password = req.body.password;
-    const salt = await bcrypt.genSalt(10); //алгорить шифрования пароля
-    const hash = await bcrypt.hash(password, salt);
-
-    //док. на пользователя
-    const doc = new UserModel({
-      email: req.body.email,
-      fullName: req.body.fullName,
-      avatarUrl: req.body.avatarUrl,
-      passwordHash: hash,
-    });
-
-    const user = await doc.save();
-
-    const token = jwt.sign(
-      {
-        _id: user._id,
-      },
-      "secret123",
-      {
-        expiresIn: 30, //срок жизни токена
-      }
-    );
-
-    const { passwordHash, ...userData } = user._doc;
-
-    res.json({
-      ...userData,
-      token,
-    });
-  } catch (err) {
-    console.warn(err);
-    res.status(500).json({
-      //500 - статус ошибки
-      message: "Some registration error",
-    });
-  }
-});
+app.post("/auth/register", registerValidator, UserControllers.register);
 
 //авторизация
-app.post("/auth/login", async (req, res) => {
-  try {
-    const user = await UserModel.findOne({ email: req.body.email });
-
-    if (!user) {
-      return res.status(404).json({
-        message: "login error!",
-      });
-    }
-
-    const isValidPass = await bcrypt.compare(
-      req.body.password,
-      user._doc.passwordHash
-    );
-
-    if (!isValidPass) {
-      return res.status(400).json({
-        message: "login error!",
-      });
-    }
-
-    const token = jwt.sign(
-      {
-        _id: user._id,
-      },
-      "secret123",
-      {
-        expiresIn: 30, //срок жизни токена
-      }
-    );
-
-    const { passwordHash, ...userData } = user._doc;
-
-    res.json({
-      ...userData,
-      token,
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({
-      message: "login error!",
-    });
-  }
-});
+app.post("/auth/login", UserControllers.login);
 
 //получение данных
-app.get("/auth/me", checkAuth, async (req, res) => {
-  try {
-    const user = await User.findById(req.userId);
-    if (!user) {
-      return res.status(404).json({
-        message: "No user",
-      });
-    }
-    res.json({
-      userData: user._doc,
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({
-      message: "login error!",
-    });
-  }
-});
+app.get("/auth/me", checkAuth, UserControllers.getMe);
 
 app.listen(port, (err) => {
   if (err) {
